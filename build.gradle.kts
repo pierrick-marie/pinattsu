9b002396ec30b8d50364921fd85687530d30f04e
Pierrick <mail@pierrickmarie.info>
2022-12-10 18:55:03 +0100

    Test rebuilt application

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,8 +32,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-
-	compileOnly("org.springframework.boot:spring-boot-devtools")
+	implementation("org.springframework.boot:spring-boot-devtools")
 
 	runtimeOnly("com.h2database:h2")
 
