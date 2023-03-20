2793fb72e45e5aaf22541149b342723df5ba6f08
Pierrick <mail@pierrickmarie.info>
2023-03-20 11:16:42 +0000

    Connect spring boot to mariadb database (https://github.com/pierrick-marie/mariadb-blog) #2

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-devtools")
+	implementation("org.mariadb.jdbc:mariadb-java-client")
 
 	runtimeOnly("com.h2database:h2")
 
