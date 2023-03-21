f39d8efdb390916be9fd4f30a1164472565db837
Pierrick <mail@pierrickmarie.info>
2023-03-21 18:11:02 +0000

    Rename package reposotory -> repository

--- src/test/kotlin/com/example/blog/repository/ArticleRepositoryTests.kt
+++ src/test/kotlin/com/example/blog/repository/ArticleRepositoryTests.kt
@@ -12,8 +12,10 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.test.context.ActiveProfiles
 
 @DataJpaTest
+@ActiveProfiles("test")
 class ArticleRepositoryTests @Autowired constructor(
 	val entityManager: TestEntityManager,
 	val articleRepository: ArticleRepository,
