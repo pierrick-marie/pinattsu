39648b8725975f8e1e01fd559975058c2f2aee56
Pierrick <mail@pierrickmarie.info>
2023-03-21 19:48:27 +0000

    Add global application test

--- src/test/kotlin/com/example/blog/BlogApplicationTests.kt
+++ src/test/kotlin/com/example/blog/BlogApplicationTests.kt
@@ -2,8 +2,10 @@ package com.example.blog
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BlogApplicationTests {
 
 	@Test
