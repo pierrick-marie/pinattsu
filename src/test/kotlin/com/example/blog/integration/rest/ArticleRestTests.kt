7a4d2833878e16ebfbbd6b37829a107cbaa781a5
Pierrick <mail@pierrickmarie.info>
2023-03-21 19:47:48 +0000

    Use H2 test profile + use real user name

--- src/test/kotlin/com/example/blog/integration/rest/ArticleRestTests.kt
+++ src/test/kotlin/com/example/blog/integration/rest/ArticleRestTests.kt
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
@@ -14,6 +15,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class ArticleRestTests @Autowired constructor(val mockMvc: MockMvc) {
 
 	@Test
@@ -22,10 +24,10 @@ class ArticleRestTests @Autowired constructor(val mockMvc: MockMvc) {
 		`Get all articles`()
 		`Post new article`()
 		`Post new article again`()
-		`Get all articles with new`()
-		`Delete first article`()
+		`Get all first articles and lastly added`()
+		`Delete first article of the list`()
 		`Get new first article`()
-		`Update last article`()
+		`Update last article of the list`()
 	}
 
 	fun `Get first article`() {
@@ -33,12 +35,12 @@ class ArticleRestTests @Autowired constructor(val mockMvc: MockMvc) {
 			get("/api/article/1").accept(MediaType.APPLICATION_JSON)
 		).andExpect(status().isOk)
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("\$.title").value("My first article"))
-			.andExpect(jsonPath("\$.author.login").value("pm1"))
+			.andExpect(jsonPath("\$.title").value("Biography of Annie"))
+			.andExpect(jsonPath("\$.author.login").value("ae"))
 
 		mockMvc.perform(
-			get("/api/article/4").accept(MediaType.APPLICATION_JSON)
-		).andExpect(status().isNotFound)
+			get("/api/article/XXX").accept(MediaType.APPLICATION_JSON)
+		).andExpect(status().isBadRequest)
 	}
 
 	fun `Get all articles`() {
@@ -46,65 +48,65 @@ class ArticleRestTests @Autowired constructor(val mockMvc: MockMvc) {
 			get("/api/article").accept(MediaType.APPLICATION_JSON)
 		).andExpect(status().isOk)
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("\$.[0].title").value("My first article"))
-			.andExpect(jsonPath("\$.[0]author.login").value("pm1"))
-			.andExpect(jsonPath("\$.[1].title").value("My second article"))
-			.andExpect(jsonPath("\$.[1]author.login").value("pm1"))
-			.andExpect(jsonPath("\$.[2].title").value("The best article ever"))
-			.andExpect(jsonPath("\$.[2]author.login").value("pm2"))
+			.andExpect(jsonPath("\$.[0].title").value("Biography of Annie"))
+			.andExpect(jsonPath("\$.[0]author.login").value("ae"))
+			.andExpect(jsonPath("\$.[1].title").value("Lorem Ipsum"))
+			.andExpect(jsonPath("\$.[1]author.login").value("ae"))
+			.andExpect(jsonPath("\$.[2].title").value("Biography of Mary"))
+			.andExpect(jsonPath("\$.[2]author.login").value("mk"))
 	}
 
 	fun `Post new article`() {
 		this.mockMvc.perform(
 			post("/api/article")
-				.content("{\"title\": \"My SUPER article from PM2\",\"content\": \"Lorem Ipsum |o|\",\"author\": \"pm2\"}")
+				.content("{\"title\": \"New Biography of Mary\",\"content\": \"Mary was a wonderful woman!\",\"author\": \"mk\"}")
 				.contentType(MediaType.APPLICATION_JSON)
 		).andExpect(status().isCreated)
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$.title").value("My SUPER article from PM2"))
-			.andExpect(jsonPath("$.content").value("Lorem Ipsum |o|"))
-			.andExpect(jsonPath("$.author.login").value("pm2"))
+			.andExpect(jsonPath("$.title").value("New Biography of Mary"))
+			.andExpect(jsonPath("$.content").value("Mary was a wonderful woman!"))
+			.andExpect(jsonPath("$.author.login").value("mk"))
 
 		mockMvc.perform(
 			get("/api/article/4").accept(MediaType.APPLICATION_JSON)
 		).andExpect(status().isOk)
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("\$.title").value("My SUPER article from PM2"))
-			.andExpect(jsonPath("\$.author.login").value("pm2"))
+			.andExpect(jsonPath("\$.title").value("New Biography of Mary"))
+			.andExpect(jsonPath("\$.author.login").value("mk"))
 	}
 
 	fun `Post new article again`() {
 		this.mockMvc.perform(
 			post("/api/article")
-				.content("{\"title\": \"My SUPER article V2\",\"content\": \"Lorem Ipsum |o|\",\"author\": \"pm2\"}")
+				.content("{\"title\": \"Lorem Ipsum V2\",\"content\": \"Lorem Ipsum again\",\"author\": \"mk\"}")
 				.contentType(MediaType.APPLICATION_JSON)
 		).andExpect(status().isCreated)
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$.title").value("My SUPER article V2"))
-			.andExpect(jsonPath("$.content").value("Lorem Ipsum |o|"))
-			.andExpect(jsonPath("$.author.login").value("pm2"))
+			.andExpect(jsonPath("$.title").value("Lorem Ipsum V2"))
+			.andExpect(jsonPath("$.content").value("Lorem Ipsum again"))
+			.andExpect(jsonPath("$.author.login").value("mk"))
 
 		mockMvc.perform(
 			get("/api/article/5").accept(MediaType.APPLICATION_JSON)
 		).andExpect(status().isOk)
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("\$.title").value("My SUPER article V2"))
-			.andExpect(jsonPath("\$.author.login").value("pm2"))
+			.andExpect(jsonPath("\$.title").value("Lorem Ipsum V2"))
+			.andExpect(jsonPath("\$.author.login").value("mk"))
 	}
 
-	fun `Get all articles with new`() {
+	fun `Get all first articles and lastly added`() {
 		mockMvc.perform(
 			get("/api/article").accept(MediaType.APPLICATION_JSON)
 		).andExpect(status().isOk)
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("\$.[0].title").value("My first article"))
-			.andExpect(jsonPath("\$.[1].title").value("My second article"))
-			.andExpect(jsonPath("\$.[2].title").value("The best article ever"))
-			.andExpect(jsonPath("\$.[3].title").value("My SUPER article from PM2"))
-			.andExpect(jsonPath("\$.[4].title").value("My SUPER article V2"))
+			.andExpect(jsonPath("\$.[0].title").value("Biography of Annie"))
+			.andExpect(jsonPath("\$.[1].title").value("Lorem Ipsum"))
+			.andExpect(jsonPath("\$.[2].title").value("Biography of Mary"))
+			.andExpect(jsonPath("\$.[3].title").value("New Biography of Mary"))
+			.andExpect(jsonPath("\$.[4].title").value("Lorem Ipsum V2"))
 	}
 
-	fun `Delete first article`() {
+	fun `Delete first article of the list`() {
 		mockMvc.perform(
 			delete("/api/article/1").accept(MediaType.APPLICATION_JSON)
 		).andExpect(status().isOk)
@@ -119,26 +121,26 @@ class ArticleRestTests @Autowired constructor(val mockMvc: MockMvc) {
 			get("/api/article/2").accept(MediaType.APPLICATION_JSON)
 		).andExpect(status().isOk)
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("\$.title").value("My second article"))
-			.andExpect(jsonPath("\$.author.login").value("pm1"))
+			.andExpect(jsonPath("\$.title").value("Lorem Ipsum"))
+			.andExpect(jsonPath("\$.author.login").value("ae"))
 	}
 
-	fun `Update last article`() {
+	fun `Update last article of the list`() {
 		this.mockMvc.perform(
 			put("/api/article/5")
-				.content("{\"title\": \"Updated article title\",\"content\": \"Updated article content\",\"author\": \"pm2\"}")
+				.content("{\"title\": \"Lorem Ipsum V3\",\"content\": \"Lorem Ipsum again and again\",\"author\": \"mk\"}")
 				.contentType(MediaType.APPLICATION_JSON)
 		).andExpect(status().isOk)
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$.title").value("Updated article title"))
-			.andExpect(jsonPath("$.content").value("Updated article content"))
-			.andExpect(jsonPath("$.author.login").value("pm2"))
+			.andExpect(jsonPath("$.title").value("Lorem Ipsum V3"))
+			.andExpect(jsonPath("$.content").value("Lorem Ipsum again and again"))
+			.andExpect(jsonPath("$.author.login").value("mk"))
 
 		mockMvc.perform(
 			get("/api/article/5").accept(MediaType.APPLICATION_JSON)
 		).andExpect(status().isOk)
 			.andExpect(content().contentType(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("\$.title").value("Updated article title"))
-			.andExpect(jsonPath("\$.author.login").value("pm2"))
+			.andExpect(jsonPath("\$.title").value("Lorem Ipsum V3"))
+			.andExpect(jsonPath("\$.author.login").value("mk"))
 	}
 }
